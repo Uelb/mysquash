4deacfbbe3b9568c79a77865410e97c1b716b382
Thomas THIMOTHEE <thomas@oxynum.fr>
2013-11-22 13:14:46 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mysquash::Application.config.secret_key_base = 'f0071b877fb738dbaee175095e66e5e7905c8389013882c8c75fa312e0674fb67410fbf61b2aa9affe44c1efab25fcc0f4c01aac9155aa0c55cf0d1c964622fb'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Mysquash::Application.config.session_store :cookie_store, key: '_mysquash_session'
