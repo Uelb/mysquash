7094a8a17541eb612a2eede512a97281da6d6a46
Thomas THIMOTHEE <thomas@oxynum.fr>
2014-01-20 18:33:02 +0900

    Subdomain for confirmation url of devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,11 +79,6 @@ Mysquash::Application.configure do
   config.log_formatter = ::Logger::Formatter.new
 
   config.action_mailer.delivery_method = :sendmail
-  # Defaults to:
-  # config.action_mailer.sendmail_settings = {
-  #   location: '/usr/sbin/sendmail',
-  #   arguments: '-i -t'
-  # }
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_options = {from: 'admin@my-squash.com'}
