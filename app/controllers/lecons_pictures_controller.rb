639b511bd61a6b73634036ab4d27af2158a8d319
Thomas THIMOTHEE <thomas@oxynum.fr>
2014-01-20 18:25:24 +0900

    assets and fix on lecons picture controller

--- app/controllers/lecons_pictures_controller.rb
+++ app/controllers/lecons_pictures_controller.rb
@@ -1,7 +1,5 @@
 class LeconsPicturesController < ApplicationController
 
-	permit_params :picture, :utf8, :authenticity_token, :commit
-
 	def create
 		@picture = LeconsPicture.create(params)
 	end
