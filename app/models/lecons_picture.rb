1d0a387768289e76f56a64de474a0bcd0fa1e200
Thomas THIMOTHEE <thomas@oxynum.fr>
2014-01-23 15:40:10 +0900

    add the link for boutique, and some css to bring the footer up, to reduce the size of the numbers (of inscriptions) and to put somem style on the title of the tournament

--- app/models/lecons_picture.rb
+++ app/models/lecons_picture.rb
@@ -1,5 +1,5 @@
 class LeconsPicture < ActiveRecord::Base
-	has_attached_file :picture, :styles => { :large => "500x500>", :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+	has_attached_file :picture, :styles => { :large => "700x375>", :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
 	validates_attachment :picture, :content_type => { :content_type => ["image/jpeg", "image/gif", "image/png", "image/jpeg"] }
 	validates :picture, :attachment_presence => true
 	scope :displayed, -> {where(displayed: true)}
