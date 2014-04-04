277bb19c19582d53c6672f212dc2863474970150
Thomas THIMOTHEE <thomas@oxynum.fr>
2014-04-04 16:48:55 +0900

    fix

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -1,14 +1,29 @@
 # encoding: utf-8
 ActiveAdmin.register User do
+
+  permit_params :email, :first_name, :last_name, :licence, :association, :telephone_number
+
   filter :male, :as => :check_boxes, collection: [["Oui", true], ["Non", false]]
   filter :tournaments
   filter :email
   filter :first_name
   filter :last_name
   filter :licence
-  filter :association
+  filter :association_name
   filter :telephone_number
 
+
+  form do |f|
+    f.inputs "Details" do
+      f.input :email
+      f.input :first_name
+      f.input :last_name
+      f.input :licence
+      f.input :association_name
+      f.input :telephone_number
+    end 
+    f.actions
+  end
   
   index do
     column :email
